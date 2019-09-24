1b9a431c75c567a73de57d159ba2fa3ef82ceb22
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-09-25 01:07:22 +0900

    fix .gitignore & missing files

---
+++ src/api/peopleData.js
@@ -0,0 +1,74 @@
+export const peopleData = [
+    {
+      name: 'Linda',
+      age: 25,
+      img: "https://i.imgur.com/QZuGC10.jpg",
+      work: "Product Manager at Tinder",
+      distance: 3,
+      update_at: 1569338653
+    },
+    {
+      name: 'Lisa',
+      age: 20,
+      img: "https://i.imgur.com/1EWwp59.jpg",
+      work: "Chief Executive Officer at Tinder",
+      distance: 4,
+      update_at: 1569338653
+    },
+    {
+      name: 'Sandra',
+      age: 18,
+      img: "https://i.imgur.com/Lu3laIj.jpg",
+      work: "Chief Operating Officer at Tinder",
+      distance: 3,
+      update_at: 1569338653
+    },
+    {
+      name: 'Chloe',
+      age: 18,
+      img: "https://i.imgur.com/WgYIxhw.png",
+      work: "Chief Financial Officer at Tinder",
+      distance: 9,
+      update_at: 1569338653
+    },
+    {
+      name: 'Alexa',
+      age: 23,
+      img: "https://i.imgur.com/D0PQegY.png",
+      work: "Chief Technical Officer at Tinder",
+      distance: 2,
+      update_at: 1569338653
+    },
+    {
+      name: 'Maria',
+      age: 21,
+      img: "https://i.imgur.com/eqd5IhH.jpg",
+      work: "Chief Strategy Officer at Tinder",
+      distance: 4,
+      update_at: 1569338653
+    },
+    {
+      name: 'Emma',
+      age: 24,
+      img: "https://i.imgur.com/4F9NXPo.png",
+      work: "Chief Information Officer at Tinder",
+      distance: 3,
+      update_at: 1569338653
+    },
+    {
+      name: 'Sara',
+      age: 18,
+      img: "http://i40.tinypic.com/ofxe21.jpg",
+      work: "Chief Knowledge Officer at Tinder",
+      distance: 7,
+      update_at: 1569338653
+    },
+    {
+      name: 'Lara',
+      age: 22,
+      img: "https://i.imgur.com/HMkdN6A.jpg",
+      work: "General Manager at Tinder",
+      distance: 1,
+      update_at: 1569338653
+    }
+]
